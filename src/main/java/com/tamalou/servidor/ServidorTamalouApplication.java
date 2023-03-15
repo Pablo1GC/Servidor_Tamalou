06cbf3ff1d2f4625c0c85ffaef1d36f4e30d2ceb
Pablo García <pablo_itunes@yahoo.es>
2023-03-15 20:45:47 +0100

    Initial commit

---
+++ src/main/java/com/tamalou/servidor/ServidorTamalouApplication.java
@@ -0,0 +1,13 @@
+package com.tamalou.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorTamalouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServidorTamalouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamalou/servidor/ServidorTamalouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamalou.servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorTamalouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
